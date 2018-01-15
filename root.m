48bf23733de4d3294c0d9ea64e512985b5a355a2
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-15 18:52:48 -0200

    minor:format

--- root.m
+++ root.m
@@ -4,7 +4,7 @@ clear all;
 constants;
 parameters;
 
-rand('seed', rand_seed);
+rand('seed',rand_seed);
 
 Theta=zeros(N,T);
 for i=1:N
@@ -14,7 +14,7 @@ end
 Pc=cumsum(P,2);
 
 for i=1:N
-  Upsilon(:,:,i) = eye(N);
+  Upsilon(:,:,i)=eye(N);
 end
 
 for k=1:T-1

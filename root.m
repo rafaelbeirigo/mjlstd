03b8f26911688f5aee6274f467d74dacb9cb8d84
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-15 19:29:35 -0200

    Calculate next Upsilon

--- root.m
+++ root.m
@@ -14,11 +14,15 @@ end
 Pc=cumsum(P,2);
 
 for i=1:N
-  Upsilon(:,:,i)=eye(N);
+  Upsilon(:,:,i)=eye(size(A,1));
 end
 
 for k=1:T-1
   for i=1:N
     Theta(i,k+1)=find(Pc(Theta(i,k),:)>=rand())(1);
+  end
+
+  for i=1:N
+    Upsilon(:,:,i)=A(:,:,Theta(i,k+1))+B(:,:,Theta(i,k+1))*F_opt(:,:,Theta(i,k+1))*Upsilon(:,:,i)
   end
 end

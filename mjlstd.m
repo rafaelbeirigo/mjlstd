9ed348217006fedcb6dac77c0d30a9e44d2301a5
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-18 18:51:58 -0200

    Add valores de retorno da função

--- mjlstd.m
+++ mjlstd.m
@@ -1,4 +1,4 @@
-function [F X qtd_iter] = mjlstd(lambda,J,T,K,epsilon,N,P,As,Bs,Cs,Ds,F_opt,X_opt,seed)
+function [Fe Fee] = mjlstd(lambda,J,T,K,epsilon,N,P,As,Bs,Cs,Ds,F_opt,X_opt)
   Theta=zeros(N,K);
   for i=1:N
     Theta(i,1)=i;

3b4930106575d9042dc743387e8911f333ae1699
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-18 19:17:50 -0200

    Remover gravação dos csvs de dentro da mjlstd

--- mjlstd.m
+++ mjlstd.m
@@ -91,12 +91,4 @@ function [Fe Fee] = mjlstd(lambda,J,T,K,epsilon,N,P,As,Bs,Cs,Ds,F_opt,X_opt,seed
       end
     end
   end
-
-  Fe_avg = mean(Fe,3);
-  Fe_std = std(Fe,0,3);
-  Fee_avg = mean(Fee,3);
-  Fee_std = std(Fee,0,3);
-
-  csvwrite('Fe_avg_std.csv',[[1:size(Fe_avg,1)]',Fe_avg,Fe_std]);
-  csvwrite('Fee_avg_std.csv',[[1:size(Fee_avg,1)]',Fee_avg,Fee_std]);
 end

18df74a57fc914e8f19b1cd7bc2b7e0abde2afac
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-24 16:17:34 -0200

    Add docstring

---
+++ stabilizes.m
@@ -0,0 +1,9 @@
+function [r] = stabilizes(Fs,As,Bs)
+  %% Tests if F stabilizes (A,B) according to Lemma 3 of (1)
+  %%
+  %% (1) O. L. V. Costa and J. C. C. Aya, "Monte Carlo
+  %% TD(\lambda)-methods for the optimal control of
+  %% discrete-time Markovian jump linear systems,"
+  %% Automatica, vol. 38, pp. 217–225, 2002.
+  
+end

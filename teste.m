83eb75f41c6f706dd5e2f9dad6f8108a791308f0
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-24 19:27:17 -0200

    Add script para teste do teste de estabilizabilidade

--- teste.m
+++ teste.m
@@ -6,5 +6,6 @@ sam_parameters;
 
 T=1000;
 disp(['Utilizando F calculado por Riccati com T=',num2str(T),' iteracoes:']);
+disp('Raios espectrais:');
 Fs_riccati=riccati(T,N,As,Bs,Cs,Ds,P);
 stabilizes(Fs_riccati,As,Bs,P,N);

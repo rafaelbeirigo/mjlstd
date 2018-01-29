0dd78e65d887783a338a58b70ef5f1b6ab523a5d
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-29 12:12:25 -0200

    fix: add missing parameter

--- teste.m
+++ teste.m
@@ -8,4 +8,4 @@ T=1000;
 disp(['Utilizando F calculado por Riccati com T=',num2str(T),' iteracoes:']);
 disp('Raios espectrais:');
 Fs_riccati=riccati(T,N,As,Bs,Cs,Ds,P);
-test_stability(Fs_riccati,As,Bs,P,N);
+test_stability(lambda,F_opt,As,Bs,P,N);

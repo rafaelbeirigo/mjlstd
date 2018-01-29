ba549f25c8296340f138dbb30abec2378ab97f60
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-29 18:10:42 -0200

    Add sam_run_episode.py

---
+++ sam_run_episode.py
@@ -0,0 +1,28 @@
+import numpy as np
+from numpy import matmul
+
+
+def get_next_theta(theta_cur, P):
+    d = np.cumsum(P, 1)[theta_cur]
+    r = np.random.random()
+    return np.nonzero(d > r)[0][0]
+
+
+def sam_run_episode(T, theta_0, x_0, P, As, Bs, Fs, seed):
+    np.random.seed(seed)
+
+    thetas = np.zeros(T, dtype=int)
+    xs = np.zeros((T,) + x_0.shape)
+
+    thetas[0] = theta_0
+    xs[0] = x_0
+
+    for t in range(1, T):
+        thetas[t] = get_next_theta(thetas[t-1], P)
+
+        A = As[thetas[t-1]]
+        B = Bs[thetas[t-1]]
+        F = Fs[thetas[t-1]]
+        xs[t] = matmul((A + matmul(B, F)), xs[t-1])
+
+    return (xs, thetas)

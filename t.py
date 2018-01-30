33ef61c6c72063f219a636f05acc2b5c5184ccc7
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-01-30 18:33:19 -0200

    ...

--- t.py
+++ t.py
@@ -13,12 +13,13 @@ T = int(1e5)
 K = int(1e6)
 epsilon = 1e-6
 
-Fs = sc.F_opt
+Fs = sc.F_sub
+Xs = sc.X_riccati
 
 test_stability(sp.lambda_par, Fs, sc.As, sc.Bs, sc.P, sc.N)
 
 (Fs, Ys) = mjlstd(sp.lambda_par, J, T, K, sp.epsilon, sc.N, sc.P, sc.As,
-                  sc.Bs, sc.Cs, sc.Ds, sc.X_opt, Fs, 0, sp.c, sp.eta)
+                  sc.Bs, sc.Cs, sc.Ds, Xs, Fs, 0, sp.c, sp.eta)
 
 print(Fs)
 print(Ys)

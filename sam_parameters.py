d8132852446d9c84f1fb8b339a52eb38f1e6ae81
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-02 08:21:12 -0200

    Update sam parameters file and use it

--- sam_parameters.py
+++ sam_parameters.py
@@ -3,13 +3,14 @@ import numpy as np
 x_0 = np.array([[1], [1]])
 theta_0 = 1
 
-R = 1
-J = 200
-T = int(1e2)
-K = int(1e6)
+R = 1                           # Number of repetitions of the experiment
 
-lambda_par = 1e-1
-c = 1
+L = 20                          # Max iterations for global Y convergence
+T = int(1e6)                    # Max iterations for individual Y convergence
+K = int(1e6)                    # Max iterations for calculating the sum
+
+lambda_ = 1e-1
+c = 1e-6
 eta = 1
 
 epsilon = 1e-6
--- t.py
+++ t.py
@@ -1,28 +1,21 @@
 from mjlstd import mjlstd, Parameters
 import sam_constants as sc
+import sam_constants as sp
 from test_stability import test_stability
 from MJLS import MJLS
 
 print('wait...')
-
-L = 20
-T = int(1e6)
-K = int(1e6)
-
-lambda_ = 1e-1
-c = 1e-6
-eta = 1
-
-epsilon = 1e-6
 
 seed = 0
 
 m = MJLS(sc.As, sc.Bs, sc.Cs, sc.Ds,
          sc.P, sc.X_ric, sc.F_ric)
 
-test_stability(m, lambda_)
+p = Parameters(sp.L, sp.T, sp.K, sp.lambda_, sp.epsilon,
+               sp.c, sp.eta, sp.seed)
 
-p = Parameters(L, T, K, lambda_, epsilon, c, eta, seed)
+test_stability(m, p.lambda_)
+
 (Fs, Ys) = mjlstd(p, m)
 
 print(Ys)

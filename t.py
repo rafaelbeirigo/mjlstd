a18054c7125037a7b4c1fe304a1d3b8db6ecd140
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-02 09:02:50 -0200

    minor: fix variable names

--- t.py
+++ t.py
@@ -1,6 +1,6 @@
 from mjlstd import mjlstd, Parameters
 import sam_constants as sc
-import sam_constants as sp
+import sam_parameters as sp
 from test_stability import test_stability
 from MJLS import MJLS
 
@@ -12,7 +12,7 @@ m = MJLS(sc.As, sc.Bs, sc.Cs, sc.Ds,
          sc.P, sc.X_ric, sc.F_ric)
 
 p = Parameters(sp.L, sp.T, sp.K, sp.lambda_, sp.epsilon,
-               sp.c, sp.eta, sp.seed)
+               sp.c, sp.eta, seed)
 
 test_stability(m, p.lambda_)
 

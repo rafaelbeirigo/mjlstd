965ad695be088102458fa22547f0c57efb6a6044
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-05 18:40:14 -0200

    Add error for dimension mismatch

--- Error.py
+++ Error.py
@@ -1,3 +1,9 @@
 class Error(Exception):
     """Base class for exceptions in this module."""
     pass
+
+
+class Not3DError(Exception):
+    """Raised when array is not three-dimensional."""
+    def __init__(self, message):
+        self.message = message

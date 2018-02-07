91a827943f5720a833bed26817cbdcb792ba1e26
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-07 15:59:24 -0200

    Add test for the matrices dimensions

--- test_MJLS.py
+++ test_MJLS.py
@@ -10,12 +10,23 @@ class TestMJLS(unittest.TestCase):
                                   None, None, None)
         self.good_boy = MJLS.MJLS(sc.As, sc.Bs, sc.Cs,
                                   sc.Ds, sc.P, sc.X_ric, sc.F_ric)
+        self.good_boy = MJLS.MJLS(sc.N, sc.As, sc.Bs, sc.Cs,
+                                  sc.Ds, sc.P, sc.Xs_ric, sc.Fs_ric)
 
     def tearDown(self):
         pass
 
     def test_number_of_dimensions(self):
         pass
+    def test_NABCDXF_have_their_set_and_get_working(self):
+        self.assertIs(sc.N, self.good_boy.N)
+        self.assertIs(sc.As, self.good_boy.A)
+        self.assertIs(sc.Bs, self.good_boy.B)
+        self.assertIs(sc.Cs, self.good_boy.C)
+        self.assertIs(sc.Ds, self.good_boy.D)
+        self.assertIs(sc.Xs_ric, self.good_boy.X)
+        self.assertIs(sc.Fs_ric, self.good_boy.F)
+
 
 if __name__ == "__main__":
     unittest.main()

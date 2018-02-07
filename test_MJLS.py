6adc95c31a563307d665c2460a5567516a2b21b8
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-07 20:01:11 -0200

    Test getting each individual matrix from ABCDFX

--- test_MJLS.py
+++ test_MJLS.py
@@ -2,6 +2,7 @@ from Error import MatricesNumberError
 import sam_constants as sc
 import MJLS
 import unittest
+import numpy.testing as npt
 
 
 class TestMJLS(unittest.TestCase):
@@ -38,6 +39,14 @@ class TestMJLS(unittest.TestCase):
         self.assertIs(sc.Ds, self.good_boy.D)
         self.assertIs(sc.Xs_ric, self.good_boy.X)
         self.assertIs(sc.Fs_ric, self.good_boy.F)
+
+    def test_get_individual_ABCDFX(self):
+        for i in range(sc.N):
+            ABCD = self.good_boy.get_ABCD(i)
+            npt.assert_array_equal(sc.As[i], ABCD[0])
+            npt.assert_array_equal(sc.Bs[i], ABCD[1])
+            npt.assert_array_equal(sc.Cs[i], ABCD[2])
+            npt.assert_array_equal(sc.Ds[i], ABCD[3])
 
 
 if __name__ == "__main__":

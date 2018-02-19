9856640e36ad174f886b9c5bb7f185899007c718
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-19 14:15:33 -0300

    Add files from email
    Date: Mon, 19 Feb 2018 14:03:06 -0300 (10 minutes, 46 seconds ago)

--- thrd.m
+++ thrd.m
@@ -1,12 +1,11 @@
-close all
 clear all
 
-lmb = 0.1;    %lambda
-epY = 1e-4;  %tolerance for convergence of Y
-epF = 1e-4;  %tolerance for convergence of F
+lmb = 0.1;   %lambda
+epY = 1e-2;  %tolerance for convergence of Y
+epF = 1e-3;  %tolerance for convergence of F
 c = 1;       %constant that multiplies gamma
-elmax = 5;  %maximum number of policy improvements
-kmax = 50;   %lookahead horizon
+elmax = 20;   %maximum number of policy improvements
+kmax = 10;   %lookahead horizon
 tmax = 1000; %maximum iterates allowed to Y(t)
 
 N = 3;
@@ -30,7 +29,7 @@ end
 %F(:,:,1) = [2.4854  -2.2734]; F(:,:,2) = [4.3655  -4.4401]; F(:,:,3) = [-5.2799   6.0555];
 
 %actually optimal controller
-%F(:,:,1) = [2.6819 -2.6721]; F(:,:,2) = [4.4230 -3.9265]; F(:,:,3) = [-5.4260 6.0883]; 
+Fop(:,:,1) = [2.6819 -2.6721]; Fop(:,:,2) = [4.4230 -3.9265]; Fop(:,:,3) = [-5.4260 6.0883]; 
 
 Q(:,:,1) = [3.6 -3.8; -3.8 4.87];
 Q(:,:,2) = [10 -3; -3 8];
@@ -52,8 +51,9 @@ R(:,:,3) = 1.111;
 
 
 Y = zeros(nx,nx,N);
-Yh = [];
-Fh = [];
+Yh = Y;
+Fh = F;
+erFh = [];
 
 el = 0;
 erF = 2*epF;
@@ -67,12 +67,12 @@ while (el<elmax && erF>epF)
       gam = c/t;
       erY = 0;
       for i=1:N
-         AA = A(:,:,i) + B(:,:,i)*F(:,:,i);
-         QQ = Q(:,:,i) + F(:,:,i)'*R(:,:,i)*F(:,:,i);
          th = hmmgenerate(kmax+2, [[zeros(1,i),1,zeros(1,N-i)];[zeros(N,1),P]],eye(N+1))-1;
          Dsum = 0;
          Up = eye(nx);
          for k=0:kmax
+            AA = A(:,:,th(k+2)) + B(:,:,th(k+2))*F(:,:,th(k+2));
+            QQ = Q(:,:,th(k+2)) + F(:,:,th(k+2))'*R(:,:,th(k+2))*F(:,:,th(k+2));
             Dk = lmb^k*Up'*(QQ + AA'*Y(:,:,th(k+2))*AA - Y(:,:,th(k+1)))*Up;
             Dsum = Dsum + Dk;
             Up = AA*Up;
@@ -92,9 +92,13 @@ while (el<elmax && erF>epF)
    end
    F = newF;
    Fh(:,:,:,end+1) = F;
+   if (isempty(erFh))
+      erFh = [erF erF];
+   else
+      erFh(end+1) = erF;
+   end
 end
 
-fprintf('erF = %f\n',erF);
 
 %Riccati verification
 erY = 2*epY;
@@ -120,61 +124,32 @@ for i=1:N
    FF(:,:,i) = aux\B(:,:,i)'*Yric(:,:,i)*A(:,:,i);
 end
 
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % PLOTS %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+ftsiz = 6;
+
 figure(1)
 clf
+
+pltnum = 320;
+for i=1:N
+   for rw=1:1
+      for cl=1:nx
+         pltnum = pltnum + 1;
+         subplot(pltnum);
+         set(gca,'FontSize',ftsiz)
+         hold on
+         stairs(0:size(Fh,4)-1, reshape(Fh(rw,cl,i,:),1,size(Fh,4)),'b')
+         plot(0:size(Fh,4)-1, FF(rw,cl,i,:)*ones(1,size(Fh,4)),'r--')
+         hold off
+         set(gca,'XLim',[0 size(Fh,4)-1])
+         ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
+         xlabel('el-step');
+      end
+   end
+end
 suptitle('Entries of F at each el-step (blue) vs true optimal gain (red)');
-
-subplot(321)
-rw = 1; cl = 1; i = 1;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
-
-subplot(322)
-rw = 1; cl = 2; i = 1;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
-
-subplot(323)
-rw = 1; cl = 1; i = 2;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
-
-subplot(324)
-rw = 1; cl = 2; i = 2;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
-
-subplot(325)
-rw = 1; cl = 1; i = 3;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
-
-subplot(326)
-rw = 1; cl = 2; i = 3;
-plot(1:size(Fh,4), [reshape(Fh(rw,cl,i,:),1,size(Fh,4));...
-      FF(rw,cl,i,:)*ones(1,size(Fh,4))])
-set(gca,'XLim',[1 size(Fh,4)])
-ylabel(sprintf('F_%d(%d,%d)',i,rw,cl));
-xlabel('el-step');
 
 %----------------------------------------------------------%
 figure(2)
@@ -187,7 +162,11 @@ for i=1:N
       for cl=rw:nx
          pltnum = pltnum + 1;
          subplot(pltnum);
-         plot(1:size(Yh,4), [reshape(Yh(rw,cl,i,:),1,size(Yh,4));Yric(rw,cl,i)*ones(1,size(Yh,4))])
+         set(gca,'FontSize',ftsiz)
+         hold on
+         plot(1:size(Yh,4), reshape(Yh(rw,cl,i,:),1,size(Yh,4)),'b')
+         plot(1:size(Yh,4), Yric(rw,cl,i)*ones(1,size(Yh,4)),'r--')
+         hold off
          set(gca,'XLim',[1 size(Yh,4)]);
          ylabel(sprintf('Y_%d(%d,%d)',i,rw,cl));
          xlabel('(t,el)-step');
@@ -197,4 +176,52 @@ end
 
 %%print 'fig2' -dpdf
 %%!ps2pdf -dEPSCrop fig2.eps fig2.pdf
+
+%----------------------------------------------------------%
+figure(3)
+clf
+suptitle('Entries of \Delta (see the 2002 Automatica paper, at the end of Sec. 5)')
+
+erFop = zeros(1,size(Fh,4));
+Delt = [];
+for t=1:size(Fh,4)
+   aux = 0;
+   for i=1:N
+      aux = max(aux, norm(Fh(:,:,i,t)-Fop(:,:,i)));
+   end
+   erFop(t) = aux;
+
+   Delt(:,:,:,end+1) = zeros(size(F));
+   for i=1:N
+      Delt(:,:,i,end) = 100*abs( (Fh(:,:,i,t)-Fop(:,:,i))./Fop(:,:,i) );
+   end
+end
+
+pltnum = 320;
+for i=1:N
+   for rw=1:1
+      for cl=1:nx
+         pltnum = pltnum + 1;
+         subplot(pltnum);
+         set(gca,'FontSize',ftsiz)
+         hold on
+         stairs(0:size(Delt,4)-1, reshape(Delt(rw,cl,i,:),1,size(Delt,4)),'b')
+         plot(0:size(Delt,4)-1, ones(1,size(Delt,4)),'k:')
+         hold off
+         set(gca,'XLim',[0 size(Delt,4)-1])
+         ylabel(sprintf('\\Delta_%d(%d,%d)',i,rw,cl));
+         xlabel('el-step');
+         if (isempty(intersect(get(gca,'YTick'),1)))
+            set(gca,'YTick',sort([1 get(gca,('YTick'))]))
+         end
+      end
+   end
+end
+
+for i=1:gcf
+   figure(i)
+   aux = sprintf('fig%d',i);
+   print('-depsc2',aux)
+   eval(sprintf('!ps2pdf -dEPSCrop %s.eps %s.pdf\n',aux,aux))
+end
 

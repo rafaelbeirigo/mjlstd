5a4714ec0324479d3b616d57a6dbfa2309afa839
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-19 12:32:54 -0300

    Use Marcos' parameters for comparison

--- sam_parameters.py
+++ sam_parameters.py
@@ -5,12 +5,12 @@ theta_0 = 1
 
 R = 1                           # Number of repetitions of the experiment
 
-L = 25                           # Max iterations for global Y convergence
-T = 100                         # Max iterations for individual Y convergence
-K = 1000                         # Max iterations for calculating the sum
+L = 5                            # Max iterations for global Y convergence
+T = 1000                         # Max iterations for individual Y convergence
+K = 50                           # Max iterations for calculating the sum
 
 lambda_ = 1e-1
-c = 0.1
-eta = 1
+c = 1.
+eta = 1.
 
-epsilon = 1e-6
+epsilon = 1e-4

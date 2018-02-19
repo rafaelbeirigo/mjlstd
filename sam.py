18a8005dd1627f158262f04597b29790c007607c
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-19 13:05:38 -0300

    Add function to plot X,Y-related data

--- sam.py
+++ sam.py
@@ -24,6 +24,49 @@ def save(data, filename):
         pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
 
 
+def plot_Y_H(m, Ys_H, X_ric, F_ric, factor):
+    plt.figure()
+
+    plt.suptitle('Entries of Y at each t,el-step (blue) '
+                 'vs true Riccati solution (red)')
+
+    # Pairs (("indexes on X"), ("index on the plot function"))
+    plot = [
+        ((0, 0, 0), (1)),
+        ((0, 0, 1), (2)),
+        ((0, 1, 1), (3)),
+        ((1, 0, 0), (4)),
+        ((1, 0, 1), (5)),
+        ((1, 1, 1), (6)),
+        ((2, 0, 0), (7)),
+        ((2, 0, 1), (8)),
+        ((2, 1, 1), (9)),
+    ]
+
+    for p in plot:
+        # Get the Ys indexes
+        i, j, k = p[0][0], p[0][1], p[0][2]
+
+        # Get the actual values to plot
+        Ys_plot = [y[i][j][k] for y in Ys_H]
+        X_plot = [X_ric[i][j][k] for y in Ys_H]
+
+        # Create the suplot
+        plt.subplot(3, 3, p[1])
+        plt.plot(Ys_plot, 'blue')
+        plt.plot(X_plot, 'red')
+        # Configure plot
+        plt.ylabel(r'$Y_{}({}, {})$'.format(i +1, j + 1, k + 1))
+        plt.xlabel('(t,el)-step')
+        plt.grid(True)
+        plt.tight_layout(h_pad=0.,w_pad=0.,pad=2)
+
+    plt.show()
+    plt.savefig('k_0_D_{:06.2f}_c_0.1.png'.format(factor),
+                bbox_inches='tight')
+    plt.close()
+
+
 def main():
     """Runs the TD(\lambda) algorithm for the Samuelson problem."""
     print('wait for it...')

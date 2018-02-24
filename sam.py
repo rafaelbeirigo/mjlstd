878f39eefa35732f9ffb386b07bc2d6042e5316a
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-02-24 17:49:03 -0300

    Add data persistence

--- sam.py
+++ sam.py
@@ -376,6 +376,9 @@ def main():
     plot_Delta_Y_sum(m, Ys_H_, Ys_el_H_, X_ric, F_ric)
     plot_Delta_F_sum(m, Fs_H_, Fs_el_H_, F_ric)
 
+    data = (m, X_ric, F_ric, Ys_H_, Ys_el_H_, Fs_H_, Fs_el_H_)
+    save(data, 'sam.pickle')
+
     call(['cp', 'sam_Y.pdf',
           '/home/rafaelbeirigo/papers/2018mjlstdon/fig/sam_Y.pdf'])
 

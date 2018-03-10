da32ba8d8413dda066cfeb9a4cb23905802e2ee0
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-03-10 11:28:22 -0300

    Progress output for L (eligibility)

--- mjlstd_eligibility.py
+++ mjlstd_eligibility.py
@@ -151,6 +151,9 @@ def mjlstd_eligibility(p, m):
     np.random.seed(p.seed)
 
     for l in range(p.L):
+        print('mjlstd_eligibility.py: L {:3d} of {:3d} '
+              '({:3.0f}%)'.format(l + 1, p.L, 100. * (l + 1)/p.L))
+
         (Ys, Ys_h) = get_Y(p, m, Fs.copy(), Ys.copy(), [])
         Fs = get_F(m, Fs.copy(), Ys.copy())
 

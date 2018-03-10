5cc83b40213fc028b765c13b494c160b7343a148
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-03-10 09:29:52 -0300

    Use dedicated file to arm's parameters

---
+++ arm_parameters.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+x_0 = np.array([[1], [1]])
+theta_0 = 1
+
+R = 1                            # Number of repetitions of the experiment
+
+L = 200                          # Max iterations for global Y convergence
+T = 100                          # Max iterations for individual Y convergence
+K = 10                           # Max iterations for calculating the sum
+
+lambda_ = 1e-1
+c = 1e-1
+eta = 1e0
+
+epsilon = 1e-3

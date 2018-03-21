61d4b263b37572006774d2414d665c9f22d0100d
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-03-21 17:24:47 -0300

    Add file.py

---
+++ file.py
@@ -0,0 +1,16 @@
+import pickle
+
+
+def load(filename):
+    """Loads persisted data."""
+    try:
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
+    except Exception:
+        return None
+
+
+def save(data, filename):
+    """Persists data."""
+    with open(filename, 'wb') as f:
+        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)

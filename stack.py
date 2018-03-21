b4d45b9171d6fe0046d29d58eb29b4a7c43e5e02
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-03-21 12:39:20 -0300

    Add stack.py

---
+++ stack.py
@@ -0,0 +1,14 @@
+def stack(a):
+    '''Stacks a list of matrices in a 1-D list.'''
+    c = []                      # shapes (cols and rows)
+    d = []                      # data in row format
+    count = 0
+    for b in a:
+        count = count + 1
+
+        c.append(b.shape[0])
+        c.append(b.shape[1])
+
+        d = d + b.reshape(-1).tolist()
+
+    return [-1] + [count] + c + d

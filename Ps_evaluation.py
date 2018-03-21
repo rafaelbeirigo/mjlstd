dcc1490c3a2eaa3e9db054f9ae0110942671b4e2
Rafael Beirigo <rafaelbeirigo@gmail.com>
2018-03-21 18:24:29 -0300

    Calculate one F for each P

--- Ps_evaluation.py
+++ Ps_evaluation.py
@@ -4,55 +4,53 @@ import arm_constants as ac
 from file import load
 from riccati import riccati
 from scipy.io import savemat
-# from arm_constants import P as P_hardcoded
+from arm_constants import P as P_hardcoded
 
+# Pasta com coisas relacionadas à obtenção por
+# contagem/maximização de verossimilhança (2017count)
 dir = 'count/'
 
-# P foi obtida de
+# P.pkl e P_tilde.pkl foram obtidas de
 # cerob/MarkovianSimulator/riccati/1000_iter/loop_loop_iteration_0100.mat
 # Git: origin/plot c88519f4d18a719f0fd1401096c22b7600762187
-P = load(dir + 'P.pkl')
-# P_tilde = load(dir + 'P_tilde.pkl')
-Fvec_tilde = load(dir + 'Fvec_tilde.pkl')
+# P_hardcoded foi encontrada no código fonte (consigo ser mais específico?)
+Ps = (('P', load(dir + 'P.pkl')),
+      ('P_tilde', load(dir + 'P_tilde.pkl')),
+      ('P_hardcoded', P_hardcoded))
 
-# O que acontece se eu
-# 1. Calcular F a partir de P no Python
-# 2. Stack essa F e salvar como .mat pelo Python
-# 3. Abrir esse .mat no Matlab, recuperando a F
-# 4. Plotar o controle a partir dessa F
-# 5. Comparar com o controle utilizando a F do Matlab (a que
-#    está salva junto com aP utilizada nesse experimento)
+for name, P in Ps:
+    # Calcular F a partir de P no Python
+    args = {
+        'T': int(1e6),
+        'N': ac.N,
+        'A': ac.A,
+        'B': ac.B,
+        'C': ac.C,
+        'D': ac.D,
+        'R': P,
+        'epsilon': 1e-6,
+    }
+    [F, _] = riccati(**args)
 
-# 1. Calcular F a partir de P no Python
-args = {
-    'T': int(1e6),
-    'N': ac.N,
-    'A': ac.A,
-    'B': ac.B,
-    'C': ac.C,
-    'D': ac.D,
-    'R': ac.P,
-    'epsilon': 1e-6,
-}
-[F, _] = riccati(**args)
+    # Zerar as devidas colunas. Isso é necessário por causa
+    # da possibilidade de falhas e à estrutura das matrizes
+    # do MJLS, que possibilitam divergência caso as devidas
+    # colunas não sejam zeradas
+    i = 0
+    for f in F:
+        if i >= 8:
+            if i <= 15:
+                f[:, 0] *= 0.
+                f[:, 3] *= 0.
+            else:
+                f[:, 1] *= 0.
+                f[:, 4] *= 0.
+        i += 1
 
-i = 0
-for f in F:
-    # Zero the adequate columns
-    if i >= 8:
-        if i <= 15:
-            f[:, 0] *= 0.
-            f[:, 3] *= 0.
-        else:
-            f[:, 1] *= 0.
-            f[:, 4] *= 0.
-    i += 1
+    F = stack(F)
+    F = np.reshape(np.array(F), (len(F), 1))
 
-# 2. Stack essa F e salvar como .mat pelo Python
-F = stack(F)
-F = np.reshape(np.array(F), (len(F), 1))
-
-folder = '/home/rafaelbeirigo/cerob/MarkovianSimulator/Ps_evaluation/P'
-filename = 'loop_loop_iteration_0001.mat'
-path = '{:s}/{:s}'.format(folder, filename)
-savemat(path, mdict={'Fvec_tilde': F})
+    folder = '/home/rafaelbeirigo/cerob/MarkovianSimulator/Ps_evaluation/' + name
+    filename = 'loop_loop_iteration_0001.mat'
+    path = '{:s}/{:s}'.format(folder, filename)
+    savemat(path, mdict={'Fvec_tilde': F})
